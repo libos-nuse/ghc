4526940a5397d608a95307797ae4251b5e87171b
Richard Eisenberg <eir@cis.upenn.edu>
2016-04-22 22:28:35 -0400

    Fix #11974 by adding a more smarts to TcDefaults.
    Test case: typecheck/should_compile/T11974

--- compiler/typecheck/TcDefaults.hs
+++ compiler/typecheck/TcDefaults.hs
@@ -13,8 +13,9 @@ import Class
 import TcRnMonad
 import TcEnv
 import TcHsType
+import TcHsSyn
 import TcSimplify
-import TcMType
+import TcValidity
 import TcType
 import PrelNames
 import SrcLoc
@@ -46,11 +47,16 @@ tcDefaults [L _ (DefaultDecl [])]
 tcDefaults [L locn (DefaultDecl mono_tys)]
   = setSrcSpan locn                     $
     addErrCtxt defaultDeclCtxt          $
-    do  { ovl_str <- xoptM LangExt.OverloadedStrings
+    do  { ovl_str   <- xoptM LangExt.OverloadedStrings
+        ; ext_deflt <- xoptM LangExt.ExtendedDefaultRules
         ; num_class    <- tcLookupClass numClassName
-        ; is_str_class <- tcLookupClass isStringClassName
-        ; let deflt_clss | ovl_str   = [num_class, is_str_class]
-                         | otherwise = [num_class]
+        ; deflt_str <- if ovl_str
+                       then mapM tcLookupClass [isStringClassName]
+                       else return []
+        ; deflt_interactive <- if ext_deflt
+                               then mapM tcLookupClass interactiveClassNames
+                               else return []
+        ; let deflt_clss = num_class : deflt_str ++ deflt_interactive
 
         ; tau_tys <- mapM (tc_default_ty deflt_clss) mono_tys
 
@@ -63,10 +69,10 @@ tcDefaults decls@(L locn (DefaultDecl _) : _)
 
 tc_default_ty :: [Class] -> LHsType Name -> TcM Type
 tc_default_ty deflt_clss hs_ty
- = do   { ty <- solveEqualities $
-                tcHsLiftedType hs_ty
-        ; ty <- zonkTcType ty   -- establish Type invariants
-        ; checkTc (isTauTy ty) (polyDefErr hs_ty)
+ = do   { (ty, _kind) <- solveEqualities $
+                         tcLHsType hs_ty
+        ; ty <- zonkTcTypeToType emptyZonkEnv ty   -- establish Type invariants
+        ; checkValidType DefaultDeclCtxt ty
 
         -- Check that the type is an instance of at least one of the deflt_clss
         ; oks <- mapM (check_instance ty) deflt_clss
@@ -90,10 +96,6 @@ dupDefaultDeclErr (L _ (DefaultDecl _) : dup_things)
   where
     pp (L locn (DefaultDecl _)) = text "here was another default declaration" <+> ppr locn
 dupDefaultDeclErr [] = panic "dupDefaultDeclErr []"
-
-polyDefErr :: LHsType Name -> SDoc
-polyDefErr ty
-  = hang (text "Illegal polymorphic type in default declaration" <> colon) 2 (ppr ty)
 
 badDefaultTy :: Type -> [Class] -> SDoc
 badDefaultTy ty deflt_clss
