8df97c91e0c9373f4827f66a384a07e12203f404
Hajime Tazaki <thehajime@gmail.com>
2016-05-02 14:57:22 +0900

    fix to build ghc head (v8.1)
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+sed "s/#BuildFlavour  = quick-cross/BuildFlavour  = quick-cross/" mk/build.mk.sample > mk/build.mk
+sed -i "s/ -fllvm//" mk/flavours/quick-cross.mk
+# XXX: don't know but ghc-head around v8.1.20 on circleci gives 
+# error for libraries/base/configure due to this flag
+sed -i "s/-Werror=unused-but-set-variable//" mk/warnings.mk
+
 ./boot
-sed "s/#BuildFlavour  = quick-cross/BuildFlavour  = quick-cross/" mk/build.mk.sample | sed "s/ -fllvm//" > mk/build.mk
 ./configure --target=x86_64-rumprun-linux --prefix=`pwd`/inst
 # XXX: some submodules are not happy with rumprun-cc cross compiling
 sed -i "s/cross_compiling=no/cross_compiling=yes/" libraries/unix/configure
 sed -i "s/cross_compiling=no/cross_compiling=yes/" libraries/base/configure
-make && make install
+make -j && make install
